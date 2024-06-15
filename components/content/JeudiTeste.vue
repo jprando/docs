dcbf0e5d0b304c85dc1c72f85eb335425b7b1a76
Jeudi Prando <jeudiprando@gmail.com>
2024-06-15 12:38:47 -0300

    feat(componente): :tada: adicionado componente JeudiTeste

---
+++ components/content/JeudiTeste.vue
@@ -0,0 +1,19 @@
+<template>
+  ... inicio: componente vue :inicio ...
+  <div>
+    <code>#componentevue</code>
+    <div>{{ mensagem }}</div>
+  </div>
+  <div>
+    <ContentSlot name="actions"></ContentSlot>
+  </div>
+  ... fim: componente vue :fim ...
+</template>
+
+<script lang="ts" setup>
+defineProps<{ mensagem: string }>();
+</script>
+
+<style>
+
+</style>
---
+++ content/teste.md
@@ -0,0 +1,20 @@
+---
+title: 'testando titulo...'
+description: 'testando descricao...'
+---
+
+inicio da pagina
+
+::jeudi-teste
+---
+mensagem: prop mensagem tah funcionando!!
+---
+
+#actions
+::u-button
+botao no slot actions
+::
+
+::
+
+fim pagina
