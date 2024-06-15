1ac66100e84c7395d492a8211dfa43a0fe963894
Jeudi Prando Araujo <jeudiprando@gmail.com>
2024-06-15 11:29:34 -0300

    chore(app): initial commit by Nuxt Studio

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@nuxthq/studio',
+    'nuxt-og-image'
+  ],
+  hooks: {
+    // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
+    'components:extend': (components) => {
+      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+
+      globals.forEach(c => c.global = true)
+    }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons']
+  },
+  colorMode: {
+    disableTransition: true
+  },
+  routeRules: {
+    '/': { prerender: true },
+    '/api/search.json': { prerender: true }
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
